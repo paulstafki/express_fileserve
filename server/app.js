f31fd2c07cef2ef1af43c746ab90bc836d456623
paulstafki <paulstafki@gmail.com>
2015-07-29 15:12:12 -0500

    fixed file error

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/*', function(req, res){
-    var file = req.params[0] || "./public/views/index.html";
+    var file = req.params[0] || "./views/index.html";
     res.sendFile(path.join(__dirname, "./public/", file));
 });
 
